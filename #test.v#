7c1705b570514a060f0a3396655e7aa30f12fb9a
Shauray <shauray9@gmail.com>
2021-03-12 11:07:35 +0530

    proving sqrt(2) is irrational

---
+++ #test.v#
@@ -0,0 +1,11 @@
+Require Import ssreflect.
+
+Variable P:Prop.
+
+Theorem test: P -> P.
+
+Proof.
+  done.
+Qed.
+
+Check test.
---
+++ irrational.py
@@ -0,0 +1,13 @@
+# p/q = sqrt(2)
+# p*p/q*q = 2
+# p*p = 2*q*q
+
+p = 1
+while 1:
+    for q in range(1, p+1):
+        if p*p == 2*q*q:
+            print(f"FUCKIN FOUND {p}/{q}")
+            exit(0)
+        else:
+            print(f"NOT FOUND {p}/{q}")
+        p += 1
